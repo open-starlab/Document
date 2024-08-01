15a32119c8cea7d942188e71d98f6f93ee1d686c
calvinyeungck <calvinyeungck@gmail.com>
2024-08-02 01:24:28 +0900

    update

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Open-STAR-Lab'
 copyright = '2024,  Openstarlab'
 author = 'Calvin Yeung'
-release = 'v1'
+release = 'v0.1.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
