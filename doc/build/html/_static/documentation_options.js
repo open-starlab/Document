6229f9c8bfcf2673c897ed6b466595769896bdde
calvinyeungck <calvinyeungck@gmail.com>
2025-02-07 19:34:53 +0900

    update

--- doc/build/html/_static/documentation_options.js
+++ doc/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.24',
+    VERSION: 'v0.1.26',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Open-STAR-Lab'
 copyright = '2024,  Openstarlab'
 author = 'Calvin Yeung'
-release = 'v0.1.25' 
+release = 'v0.1.26' 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
